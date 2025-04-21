a8e2a83fbb50de20eaad1b0486b5d06f11bf1e68
Nowshin <82333564+Nyctophilia58@users.noreply.github.com>
2025-04-21 17:39:36 +0600

    Update apikey.dart

--- lib/constants/apikey.dart
+++ lib/constants/apikey.dart
@@ -1,1 +1,1 @@
-String api_key = "90b531c6171bd68e7aee864d15926141";  // Replace with your OpenWeather API key
+String api_key = "Your OpenWeather Key Here";  // Replace with your OpenWeather API key
